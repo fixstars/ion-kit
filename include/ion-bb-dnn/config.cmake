96aa5698fd879a0807eb62ad5809e095dc2d3dbb
Takuro Iizuka <t_iizuka@fixstars.com>
2020-11-03 19:54:57 -0800

    Temporary disabled

--- include/ion-bb-dnn/config.cmake
+++ include/ion-bb-dnn/config.cmake
@@ -3,28 +3,31 @@ if(HALIDE_ROOT STREQUAL "")
     message(FATAL_ERROR "Set appropriate path to Halide")
 endif()
 
-find_package(OpenCV 3 REQUIRED)
-add_compile_options(-Wno-format-security)
+# find_package(OpenCV 3 REQUIRED)
+# add_compile_options(-Wno-format-security)
 
-set(ONNXRUNTIME_ROOT $ENV{ONNXRUNTIME_ROOT} CACHE PATH "Path to onnxruntime")
-if(ONNXRUNTIME_ROOT STREQUAL "")
-    message(FATAL_ERROR "Set appropriate path to onnxruntime")
-endif()
+# set(ONNXRUNTIME_ROOT $ENV{ONNXRUNTIME_ROOT} CACHE PATH "Path to onnxruntime")
+# if(ONNXRUNTIME_ROOT STREQUAL "")
+#     message(FATAL_ERROR "Set appropriate path to onnxruntime")
+# endif()
 
 set(INCLUDE_DIRS
     ${HALIDE_ROOT}/include
-    ${ONNXRUNTIME_ROOT}/include
-    ${OpenCV_INCLUDE_DIRS})
+    #${ONNXRUNTIME_ROOT}/include
+    #${OpenCV_INCLUDE_DIRS}
+    )
 
 set(LINK_DIRS
     ${HALIDE_ROOT}/bin
-    ${ONNXRUNTIME_ROOT}/lib
-    ${OpenCV_DIR}/lib)
+    #${ONNXRUNTIME_ROOT}/lib
+    #${OpenCV_DIR}/lib
+    )
 
 set(RUNTIME_ENVS
     LD_LIBRARY_PATH ${HALIDE_ROOT}/bin
-    LD_LIBRARY_PATH ${OpenCV_DIR}/lib
-    LD_LIBRARY_PATH ${ONNXRUNTIME_ROOT}/lib)
+    #LD_LIBRARY_PATH ${OpenCV_DIR}/lib
+    #LD_LIBRARY_PATH ${ONNXRUNTIME_ROOT}/lib
+    )
 
 set(LIBRARIES
     rt
@@ -33,4 +36,5 @@ set(LIBRARIES
     m
     z
     uuid
-    ${OpenCV_LIBS})
+    #${OpenCV_LIBS}
+    )
