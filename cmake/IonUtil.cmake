778bf4d33225d882bf588625aba9432f256e144b
Akira Maruoka <akmaru0266@gmail.com>
2020-12-07 10:14:04 +0900

    Fixed library to Halide::Halide

--- cmake/IonUtil.cmake
+++ cmake/IonUtil.cmake
@@ -23,7 +23,7 @@ function(ion_compile NAME)
             PUBLIC -rdynamic) # For JIT compiling
     endif()
     target_include_directories(${NAME} PUBLIC "${PROJECT_SOURCE_DIR}/include;${ION_BB_INCLUDE_DIRS};${HALIDE_INCLUDE_DIR}")
-    target_link_libraries(${NAME} ion-core ${HALIDE_LIBRARY} ${ION_BB_LIBRARIES} ${PLATFORM_LIBRARIES})
+    target_link_libraries(${NAME} ion-core Halide::Halide ${ION_BB_LIBRARIES} ${PLATFORM_LIBRARIES})
     set_target_properties(${NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY compile PIPELINE_NAME ${IEC_PIPELINE_NAME})
 endfunction()
 
@@ -67,7 +67,7 @@ function(ion_run NAME COMPILE_NAME)
     # Build run
     add_executable(${NAME} ${IER_SRCS} ${HEADER})
     target_include_directories(${NAME} PUBLIC "${PROJECT_SOURCE_DIR}/include;${ION_BB_INCLUDE_DIRS};${HALIDE_INCLUDE_DIR};${OUTPUT_PATH}")
-    target_link_libraries(${NAME} ${STATIC_LIB} ${ION_BB_LIBRARIES} ${HALIDE_LIBRARY} ${PLATFORM_LIBRARIES})
+    target_link_libraries(${NAME} ${STATIC_LIB} ${ION_BB_LIBRARIES} Halide::Halide ${PLATFORM_LIBRARIES})
 
     add_test(NAME ${NAME} COMMAND $<TARGET_FILE:${NAME}> ${IER_RUNTIME_ARGS})
     set_tests_properties(${NAME} PROPERTIES ENVIRONMENT "${IER_RUNTIME_ENVS}")
@@ -95,7 +95,7 @@ function(ion_jit NAME)
         target_compile_options(${NAME} PUBLIC -rdynamic)
     endif()
     target_include_directories(${NAME} PUBLIC "${PROJECT_SOURCE_DIR}/include;${ION_BB_INCLUDE_DIRS};${HALIDE_INCLUDE_DIR}")
-    target_link_libraries(${NAME} ion-core ${HALIDE_LIBRARY} ${ION_BB_LIBRARIES} ${PLATFORM_LIBRARIES})
+    target_link_libraries(${NAME} ion-core Halide::Halide ${ION_BB_LIBRARIES} ${PLATFORM_LIBRARIES})
     set_target_properties(${NAME} PROPERTIES ENABLE_EXPORTS ON)
 endfunction()
 
