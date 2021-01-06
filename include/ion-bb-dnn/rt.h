2f65591900b822c005c7cb1388161e928fe3b890
Takuro Iizuka <t_iizuka@fixstars.com>
2021-01-06 01:50:26 -0800

    Downloading models

--- include/ion-bb-dnn/rt.h
+++ include/ion-bb-dnn/rt.h
@@ -38,10 +38,7 @@ extern "C" ION_EXPORT int ion_bb_dnn_generic_object_detection(halide_buffer_t *i
 
         using namespace ion::bb::dnn;
 
-        if (trt::is_available()) {
-            std::string session_id(reinterpret_cast<const char *>(session_id_buf->host));
-            return trt::object_detection(in, session_id, model_root_url, out);
-        } else if (is_tfl_available()) {
+        if (is_tfl_available()) {
             return object_detection_tfl(in, model_root_url, cache_root, out);
         } else if (is_ort_available()) {
             std::string session_id(reinterpret_cast<const char *>(session_id_buf->host));
@@ -62,7 +59,49 @@ extern "C" ION_EXPORT int ion_bb_dnn_generic_object_detection(halide_buffer_t *i
     }
 }
 
-#undef ION_EXPORT
+extern "C" ION_EXPORT int ion_bb_dnn_tlt_object_detection(halide_buffer_t *in,
+                                                          halide_buffer_t *session_id_buf,
+                                                          halide_buffer_t *model_root_url_buf,
+                                                          halide_buffer_t *cache_root_buf,
+                                                          halide_buffer_t *out) {
+    try {
 
+        if (in->is_bounds_query()) {
+            // Both input and output is (N)HWC
+            for (int i=0; i<in->dimensions; ++i) {
+                in->dim[i].min = out->dim[i].min;
+                in->dim[i].extent = out->dim[i].extent;
+            }
+            return 0;
+        }
+
+        Halide::Runtime::Buffer<float> in_buf(*in);
+        in_buf.copy_to_host();
+
+        std::string model_root_url(reinterpret_cast<const char *>(model_root_url_buf->host));
+        std::string cache_root(reinterpret_cast<const char *>(cache_root_buf->host));
+
+        using namespace ion::bb::dnn;
+
+        if (trt::is_available()) {
+            std::string session_id(reinterpret_cast<const char *>(session_id_buf->host));
+            return trt::object_detection_ssd(in, session_id, model_root_url, cache_root, out);
+        } else {
+            std::cerr << "No available runtime" << std::endl;
+            return -1;
+        }
+
+        return 0;
+
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    } catch (...) {
+        std::cerr << "Unknown error" << std::endl;
+        return -1;
+    }
+}
+
+#undef ION_EXPORT
 
 #endif  // ION_BB_DNN_BB_H
--- include/ion-bb-dnn/rt_trt.h
+++ include/ion-bb-dnn/rt_trt.h
@@ -1,204 +1,3 @@
-#if 0
-#include <iostream>
-#include <exception>
-#include <vector>
-#include <fstream>
-#include <cassert>
-#include <NvInfer.h>
-#include <NvInferRuntime.h>
-#include <NvInferPlugin.h>
-#include <cuda_runtime_api.h>
-
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-
-class Logger : public nvinfer1::ILogger {
- public:
-     void log(Severity severity, const char* msg) override
-     {
-         std::cerr << msg << std::endl;
-     }
-};
-
-std::vector<uint8_t> load(const std::string& path)
-{
-    std::vector<uint8_t> buffer;
-
-    std::ifstream ifs(path.c_str(), std::ios::binary);
-    if (!ifs.is_open()) {
-        throw std::runtime_error("File not found : " + path);
-    }
-
-    ifs.seekg(0, std::ifstream::end);
-    std::ifstream::pos_type end = ifs.tellg();
-
-    ifs.seekg(0, std::ifstream::beg);
-    std::ifstream::pos_type beg = ifs.tellg();
-
-    std::size_t size_in_byte = end-beg;
-
-    buffer.resize(size_in_byte);
-
-    ifs.read(reinterpret_cast<char*>(buffer.data()), size_in_byte);
-
-    return buffer;
-}
-
-int main()
-{
-    using namespace nvinfer1;
-
-    Logger logger;
-
-    auto in = cv::imread("in.png");
-
-    std::cout << "in.empty() : " << in.empty() << std::endl;
-
-    cv::Mat in2;
-    // cv::resize(in, in2, cv::Size(1248, 384), 0, 0);
-    int top  = std::max((384 - in.rows) / 2, 0);
-    int bottom = 384 - in.rows - top;
-    int left = std::max((1248 - in.cols) / 2, 0);
-    int right = 1248 - in.cols - left;
-
-    cv::copyMakeBorder(in, in2, top, bottom, left, right, cv::BORDER_CONSTANT, 0);
-
-    cv::Mat in3;
-    cv::normalize(in2, in3, 0, 255.0, cv::NORM_MINMAX, CV_32FC3);
-
-    cv::Mat in4 = in3.reshape(1, 384*1248);
-    cv::Mat in5 = in4.t();
-    // std::cout << in3.size[0] << " " << in3.size[1] << std::endl;
-    // std::cout << in4.size[0] << " " << in4.size[1] << std::endl;
-    // std::cout << in5.size[0] << " " << in5.size[1] << std::endl;
-
-    cv::Mat test;
-    cv::normalize(in5.reshape(1, 384*3), test, 0, 255, cv::NORM_MINMAX, CV_8UC1);
-    cv::imwrite("test.png", test);
-
-    auto result = initLibNvInferPlugins(nullptr, "");
-    assert(result);
-
-    auto buffer = load("trt.engine");
-
-    IRuntime *runtime = createInferRuntime(logger);
-    assert(runtime != nullptr);
-
-    ICudaEngine *engine = runtime->deserializeCudaEngine(buffer.data(), buffer.size(), nullptr);
-    assert(engine != nullptr);
-
-    std::cout << "MaxBatchSize : " << engine->getMaxBatchSize() << std::endl;
-
-#if 1
-    IExecutionContext *context = engine->createExecutionContext();
-    assert(context != nullptr);
-
-    int ret = cudaSuccess;
-    size_t size = 0;
-    std::vector<void *> buffers;
-
-    float *input = nullptr;
-    size = 3 * 384 * 1248 * sizeof(float);
-    ret = cudaMalloc(reinterpret_cast<void**>(&input), size);
-    assert(ret == cudaSuccess);
-    ret = cudaMemcpy(input, in5.ptr(), 3 * 384 * 1248 * sizeof(float), cudaMemcpyHostToDevice);
-    // ret = cudaMemset(input, 0, size);
-    assert(ret == cudaSuccess);
-    buffers.push_back(reinterpret_cast<void*>(input));
-
-    float *output0 = nullptr;
-    size = 1 * 200 * 7 * sizeof(float);
-    ret = cudaMalloc(reinterpret_cast<void**>(&output0), size);
-    assert(ret == cudaSuccess);
-    ret = cudaMemset(output0, 0, size);
-    assert(ret == cudaSuccess);
-    buffers.push_back(reinterpret_cast<void*>(output0));
-
-    float *output1 = nullptr;
-    size = 1 * 1 * 1 * sizeof(float);
-    ret = cudaMalloc(reinterpret_cast<void**>(&output1), size);
-    assert(ret == cudaSuccess);
-    ret = cudaMemset(output1, 0, size);
-    assert(ret == cudaSuccess);
-    buffers.push_back(reinterpret_cast<void*>(output1));
-
-    const int32_t batch_size = 1;
-    context->execute(batch_size, buffers.data());
-
-    std::vector<float> output0_host(1 * 200 * 7);
-    ret = cudaMemcpy(output0_host.data(), output0, 1 * 200 * 7 * sizeof(float), cudaMemcpyDeviceToHost);
-    assert(ret == cudaSuccess);
-
-    // 200 * [image_id, label, confidence, xmin, ymin, xmax, ymax]
-    for (int i=0; i<200; ++i) {
-        auto confidence = output0_host[i * 7 + 2];
-        if (confidence > 0.4) {
-            const int x1 = output0_host[i * 7 + 3] * 1284;
-            const int y1 = output0_host[i * 7 + 4] * 384;
-            const int x2 = output0_host[i * 7 + 5] * 1284;
-            const int y2 = output0_host[i * 7 + 6] * 384;
-            const cv::Point2d p1(x1, y1);
-            const cv::Point2d p2(x2, y2);
-            const cv::Scalar color = cv::Scalar(255, 0, 0);
-            cv::rectangle(in2, p1, p2, color);
-        }
-        // cv::putText(frame, label, cv::Point(x1, y1 - 3), cv::FONT_HERSHEY_COMPLEX, 0.5, color);
-
-
-        //if (output0_host[i * 7 + 2] > 0.7)  {
-        //    std::cout << output0_host[i * 7 + 2] << std::endl;
-        //}
-    }
-
-    cv::imwrite("out.png", in2);
-
-    // std::cout << output0_host[0] << std::endl;
-
-    cudaFree(input);
-    cudaFree(output0);
-    cudaFree(output1);
-
-    std::cout << "Done" << std::endl;
-
-#else
-    std::cout << engine->getNbBindings() << std::endl;
-    std::cout << engine->getBindingName(0) << std::endl;
-    {
-        auto dim = engine->getBindingDimensions(0);
-        std::cout << "type: " << static_cast<int32_t>(engine->getBindingDataType(0)) << std::endl;
-        std::cout << "[";
-        for (int i=0; i<dim.nbDims; ++i) {
-            std::cout << dim.d[i] << " ";
-        }
-        std::cout << "]" << std::endl;
-    }
-
-    std::cout << engine->getBindingName(1) << std::endl;
-    {
-        auto dim = engine->getBindingDimensions(1);
-        std::cout << "type: " << static_cast<int32_t>(engine->getBindingDataType(1)) << std::endl;
-        std::cout << "[";
-        for (int i=0; i<dim.nbDims; ++i) {
-            std::cout << dim.d[i] << " ";
-        }
-        std::cout << "]" << std::endl;
-    }
-
-    std::cout << engine->getBindingName(2) << std::endl;
-    {
-        auto dim = engine->getBindingDimensions(2);
-        std::cout << "type: " << static_cast<int32_t>(engine->getBindingDataType(2)) << std::endl;
-        std::cout << "[";
-        for (int i=0; i<dim.nbDims; ++i) {
-            std::cout << dim.d[i] << " ";
-        }
-        std::cout << "]" << std::endl;
-    }
-#endif
-return 0;
-}
-#else
-
 #ifndef ION_BB_DNN_RT_TRT_H
 #define ION_BB_DNN_RT_TRT_H
 
@@ -217,15 +16,24 @@ enum cudaMemcpyKind
     cudaMemcpyDefault             =   4       /**< Direction of the transfer is inferred from the pointer values. Requires unified virtual addressing */
 };
 
+enum cudaDeviceAttr {
+    cudaDevAttrComputeCapabilityMajor         = 75, /**< Major compute capability version number */
+    cudaDevAttrComputeCapabilityMinor         = 76, /**< Minor compute capability version number */
+};
+
 using cudaMalloc_t = int (*)(void **devPtr, size_t size);
 using cudaMemset_t = int (*)(void *devPtr, int value, size_t count);
 using cudaMemcpy_t = int (*)(void *dst, const void *src, size_t count, enum cudaMemcpyKind kind);
 using cudaFree_t = int (*)(void *devPtr);
+using cudaGetDevice_t = int (*)(int *device);
+using cudaDeviceGetAttribute_t = int (*)(int *value, enum cudaDeviceAttr attr, int device);
 
 cudaMalloc_t cudaMalloc = nullptr;
 cudaMemset_t cudaMemset = nullptr;
 cudaMemcpy_t cudaMemcpy = nullptr;
 cudaFree_t cudaFree = nullptr;
+cudaGetDevice_t cudaGetDevice = nullptr;
+cudaDeviceGetAttribute_t cudaDeviceGetAttribute = nullptr;
 
 //
 // NvInfer
@@ -283,11 +91,11 @@ class Logger : public nvinfer1::ILogger {
 
 class SessionManager {
  public:
-     static SessionManager& get_instance(const std::string& uuid, const std::string& model_root_url) {
+     static SessionManager& get_instance(const std::string& uuid, const std::string& model_root_url, const std::string& cache_root) {
          static std::map<std::string, std::unique_ptr<SessionManager>> map_;
          SessionManager *sess;
          if (map_.count(uuid) == 0) {
-             map_[uuid] = std::unique_ptr<SessionManager>(new SessionManager(model_root_url));
+             map_[uuid] = std::unique_ptr<SessionManager>(new SessionManager(model_root_url, cache_root));
          }
          return *map_[uuid].get();
      }
@@ -308,13 +116,73 @@ class SessionManager {
      }
 
  private:
-     SessionManager(const std::string& model_root_url)
+     SessionManager(const std::string& model_root_url, const std::string& cache_root)
          : cudart_dm_("cudart"), nvinfer_dm_("nvinfer"), nvinfer_plugin_dm_("nvinfer_plugin")
      {
          using namespace nvinfer1;
 
          if (!init()) {
              throw std::runtime_error("Failed to initialize runtime libraries");
+         }
+
+         int device = 0;
+         if (cudaGetDevice(&device) != 0) {
+             throw std::runtime_error("Failed to get CUDA device");
+         }
+
+         int major = 0;
+         if (cudaDeviceGetAttribute(&major, cudaDevAttrComputeCapabilityMajor, device) != 0) {
+             throw std::runtime_error("Failed to get CUDA device attribute");
+         }
+
+         int minor = 0;
+         if (cudaDeviceGetAttribute(&minor, cudaDevAttrComputeCapabilityMinor, device) != 0) {
+             throw std::runtime_error("Failed to get CUDA device attribute");
+         }
+
+         std::string model_name;
+         if (major == 7 && minor == 5) {
+             // Assumes latest x86 environment
+             model_name = "trt721_arch75_fp16.engine";
+         } else if (major == 7 && minor == 2) {
+             // Jetson NX + JetPack 4.4
+             model_name = "trt713_arch72_fp16.engine";
+         } else if (major == 5 && minor == 3) {
+             // Jetson Nano + JetPack 4.4
+             model_name = "trt713_arch53_fp16.engine";
+         } else {
+             throw std::runtime_error("Unsupported CUDA device");
+         }
+
+         std::ifstream ifs(cache_root + model_name, std::ios::binary);
+         if (ifs.is_open()) {
+             auto begin = ifs.tellg();
+             ifs.seekg(0, std::ios::end);
+             auto end = ifs.tellg();
+             ifs.seekg(0, std::ios::beg);
+             model_.resize(end-begin);
+             ifs.read(reinterpret_cast<char*>(model_.data()), model_.size());
+         } else {
+             std::string model_url = model_root_url + model_name;
+             std::string host_name;
+             std::string path_name;
+             std::tie(host_name, path_name) = parse_url(model_url);
+             if (host_name.empty() || path_name.empty()) {
+                 throw std::runtime_error("Invalid model URL : " + model_url);
+             }
+
+             httplib::Client cli(host_name.c_str());
+             cli.set_follow_location(true);
+             auto res = cli.Get(path_name.c_str());
+             if (!res || res->status != 200) {
+                 throw std::runtime_error("Failed to download model : " + model_url);
+             }
+
+             model_.resize(res->body.size());
+             std::memcpy(model_.data(), res->body.c_str(), res->body.size());
+
+             std::ofstream ofs (cache_root + model_name, std::ios::binary);
+             ofs.write(reinterpret_cast<const char*>(model_.data()), model_.size());
          }
 
          if (!initLibNvInferPlugins(nullptr, "")) {
@@ -326,9 +194,7 @@ class SessionManager {
              throw std::runtime_error("Failed to create TensorRT runtime");
          }
 
-         auto engine_data = load("trt.engine");
-
-         ICudaEngine *engine = runtime->deserializeCudaEngine(engine_data.data(), engine_data.size(), nullptr);
+         ICudaEngine *engine = runtime->deserializeCudaEngine(model_.data(), model_.size(), nullptr);
          if (engine == nullptr) {
              throw std::runtime_error("Failed to create TensorRT inference engine");
          }
@@ -388,6 +254,8 @@ class SessionManager {
         RESOLVE_SYMBOL(cudart_dm_, cudaMemset);
         RESOLVE_SYMBOL(cudart_dm_, cudaMemcpy);
         RESOLVE_SYMBOL(cudart_dm_, cudaFree);
+        RESOLVE_SYMBOL(cudart_dm_, cudaGetDevice);
+        RESOLVE_SYMBOL(cudart_dm_, cudaDeviceGetAttribute);
         RESOLVE_SYMBOL(nvinfer_dm_, createInferRuntime_INTERNAL);
         RESOLVE_SYMBOL(nvinfer_dm_, createInferRefitter_INTERNAL);
         RESOLVE_SYMBOL(nvinfer_plugin_dm_, initLibNvInferPlugins);
@@ -405,6 +273,8 @@ class SessionManager {
      std::vector<std::tuple<void*, size_t>> buffers_;
 
      Logger logger_;
+
+     std::vector<uint8_t> model_;
 };
 
 bool is_available() {
@@ -428,17 +298,18 @@ bool is_available() {
     return true;
 }
 
-int object_detection(halide_buffer_t *in,
-                     const std::string& session_id,
-                     const std::string& model_root_url,
-                     halide_buffer_t *out) {
+int object_detection_ssd(halide_buffer_t *in,
+                         const std::string& session_id,
+                         const std::string& model_root_url,
+                         const std::string& cache_root,
+                         halide_buffer_t *out) {
     using namespace nvinfer1;
 
     const int channel = 3;
     const int width = in->dim[1].extent;
     const int height = in->dim[2].extent;
 
-    auto& session = SessionManager::get_instance(session_id, model_root_url);
+    auto& session = SessionManager::get_instance(session_id, model_root_url, cache_root);
 
     // auto in_ = cv::imread("in.png");
     // std::cout << "in.empty() : " << in.empty() << std::endl;
@@ -537,4 +408,3 @@ int object_detection(halide_buffer_t *in,
 } // ion
 
 #endif // ION_BB_DNN_RT_TRT_H
-#endif
