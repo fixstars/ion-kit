5013fcf6dfebbfdc8c84d1b3fdc3a57215f2ec9e
Kenta Fujinami <kenta.fujinami@fixstars.com>
2021-01-27 18:12:34 +0900

    Add local file loader to ImageLoader

--- include/ion-bb-image-io/rt_file.h
+++ include/ion-bb-image-io/rt_file.h
@@ -13,46 +13,50 @@
 #include <opencv2/imgproc.hpp>
 
 extern "C" int ION_EXPORT ion_bb_image_io_image_loader(halide_buffer_t *in, halide_buffer_t *out) {
+    const char *url = reinterpret_cast<const char *>(in->host);
 
-    if (in->is_bounds_query()) {
-        // NOP
-    } else {
-
-        const char *url = reinterpret_cast<const char *>(in->host);
-
-        static std::unordered_map<const char *, cv::Mat> decoded;
-        if (decoded.count(url) == 0) {
-            std::string host_name;
-            std::string path_name;
-            std::tie(host_name, path_name) = ion::bb::image_io::parse_url(url);
-            if (host_name.empty() || path_name.empty()) {
-                std::cerr << "Invalid URL is specified" << std::endl;
-                return -1;
+    static std::unordered_map<const char *, cv::Mat> decoded;
+    if (decoded.count(url) == 0) {
+        std::string host_name;
+        std::string path_name;
+        std::tie(host_name, path_name) = ion::bb::image_io::parse_url(url);
+        bool img_loaded = false;
+        cv::Mat frame;
+        if (host_name.empty() || path_name.empty()) {
+            // fallback to local file
+            frame = cv::imread(url);
+            if (!frame.empty()) {
+                img_loaded = true;
             }
+        } else {
             httplib::Client cli(host_name.c_str());
             cli.set_follow_location(true);
             auto res = cli.Get(path_name.c_str());
             if (res && res->status == 200) {
                 std::vector<char> data(res->body.size());
                 std::memcpy(data.data(), res->body.c_str(), res->body.size());
-                cv::Mat frame = cv::imdecode(cv::InputArray(data), cv::IMREAD_COLOR);
-                cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
-                decoded[url] = frame;
+                frame = cv::imdecode(cv::InputArray(data), cv::IMREAD_COLOR);
+                img_loaded = true;
             }
         }
-
-        const cv::Mat &img(decoded[url]);
-
-        if (out->is_bounds_query()) {
-            out->dim[0].min = 0;
-            out->dim[0].extent = 3;
-            out->dim[1].min = 0;
-            out->dim[1].extent = img.cols;
-            out->dim[2].min = 0;
-            out->dim[2].extent = img.rows;
+        if (img_loaded) {
+            decoded[url] = frame;
         } else {
-            std::memcpy(out->host, img.data, img.total() * img.elemSize());
+            return -1;
         }
+    }
+
+    const cv::Mat &img(decoded[url]);
+
+    if (out->is_bounds_query()) {
+        out->dim[0].min = 0;
+        out->dim[0].extent = 3;
+        out->dim[1].min = 0;
+        out->dim[1].extent = img.cols;
+        out->dim[2].min = 0;
+        out->dim[2].extent = img.rows;
+    } else {
+        std::memcpy(out->host, img.data, img.total() * img.elemSize());
     }
 
     return 0;
