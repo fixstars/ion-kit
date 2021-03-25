f6bb9896f41ec476c27af746fd64190824422733
Akira Maruoka <akmaru0266@gmail.com>
2021-03-26 08:10:23 +0900

    Remove HALIDE_ROOT dependencies

--- include/ion-bb-dnn/config.cmake
+++ include/ion-bb-dnn/config.cmake
@@ -1,8 +1,3 @@
-set(HALIDE_ROOT $ENV{HALIDE_ROOT} CACHE PATH "Path to Halide")
-if(HALIDE_ROOT STREQUAL "")
-    message(FATAL_ERROR "Set HALIDE_ROOT")
-endif()
-
 # OpenCV
 find_package(OpenCV 4 REQUIRED)
 if (UNIX)
@@ -10,11 +5,9 @@ if (UNIX)
 endif()
 
 set(INCLUDE_DIRS
-    ${HALIDE_ROOT}/include
     ${OpenCV_INCLUDE_DIRS})
 
 set(LINK_DIRS
-    ${HALIDE_ROOT}/bin
     ${OpenCV_DIR}/lib)
 
 if (UNIX)
