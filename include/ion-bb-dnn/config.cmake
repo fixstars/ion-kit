52916aee4c39d7821aa76f2ddb3080afd82e5437
Takafumi Kubota <takafumi.kubota@fixstars.com>
2021-05-21 14:47:55 +0900

    [dnn/tvm] remove WITH_TVM cmake option

--- include/ion-bb-dnn/config.cmake
+++ include/ion-bb-dnn/config.cmake
@@ -4,22 +4,6 @@ if (UNIX)
     add_compile_options(-Wno-format-security)
 endif()
 
-#
-# tvm
-#
-if(NOT WITH_TVM STREQUAL "OFF")
-  message(STATUS "will use libtvm_runtime.so")
-
-  # confirm CMake can find the libtvm_runtime.so & set rpath if required
-  if (WITH_TVM STREQUAL "ON")
-      find_library(TVM_RUNTIME_LIB libtvm_runtime.so REQUIRED)
-  else()
-      find_library(TVM_RUNTIME_LIB NAMES libtvm_runtime.so PATHS ${WITH_TVM} REQUIRED)
-      get_filename_component(RPATH_DIR ${TVM_RUNTIME_LIB} DIRECTORY)
-      set(CMAKE_EXE_LINKER_FLAGS "-Wl,-rpath ${RPATH_DIR}")
-  endif()
-endif()
-
 set(INCLUDE_DIRS
     ${CMAKE_SOURCE_DIR}/include/ion-bb-dnn/3rdparty
     ${OpenCV_INCLUDE_DIRS})
