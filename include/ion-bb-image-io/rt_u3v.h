9fba1882b6255a0ca4e5689a43b0b627cfe2f6ff
Momoko Kono <momoko.kono@fixstars.com>
2023-06-16 03:44:38 +0900

    added the version log

--- include/ion-bb-image-io/rt_u3v.h
+++ include/ion-bb-image-io/rt_u3v.h
@@ -312,7 +312,7 @@ class U3V {
         frame_sync_(frame_sync), realtime_diaplay_mode_(realtime_diaplay_mode), is_gendc_(false),
         devices_(num_sensor), buffers_(num_sensor), disposed_(false)
     {
-        printf("[LOG ion-kit] This is ion-kit with debug-log; updated 23/06/08\n");
+        printf("[LOG ion-kit] This is ion-kit with debug-log; updated 23/06/15\n");
         init_symbols();
 
         arv_update_device_list();
