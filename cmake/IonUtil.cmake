28e60c2502b9cffca875368c8f6501fa95fba8af
samywang92 <samuel.wang@fixstars.com>
2023-12-21 13:43:19 -0800

    add macos support for latest master

--- cmake/IonUtil.cmake
+++ cmake/IonUtil.cmake
@@ -17,9 +17,14 @@ function(ion_compile NAME)
 
     # Build compile
     add_executable(${NAME} ${IEC_SRCS})
-    if(UNIX)
+    if(UNIX AND NOT APPLE)
         target_compile_options(${NAME} PUBLIC -fno-rtti)  # For Halide::Generator
         target_link_options(${NAME} PUBLIC -Wl,--export-dynamic) # For JIT compiling
+    endif()
+    IF (APPLE)
+        target_compile_options(${NAME}
+            PUBLIC -fno-rtti  # For Halide::Generator
+            PUBLIC -rdynamic) # For JIT compiling
     endif()
     target_include_directories(${NAME} PUBLIC "${PROJECT_SOURCE_DIR}/include")
     target_link_libraries(${NAME} PRIVATE ion-core ${PLATFORM_LIBRARIES})
@@ -103,8 +108,13 @@ function(ion_jit NAME)
     set(multiValueArgs SRCS)
     cmake_parse_arguments(IEJ "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
     add_executable(${NAME} ${IEJ_SRCS})
-    if (UNIX)
+    if (UNIX AND NOT APPLE)
         target_link_options(${NAME} PUBLIC -Wl,--export-dynamic) # For JIT compiling
+    endif()
+    if (APPLE)
+        target_compile_options(${NAME}
+            PUBLIC -fno-rtti  # For Halide::Generator
+            PUBLIC -rdynamic) # For JIT compiling
     endif()
     find_package(OpenCV 4 REQUIRED)
     target_include_directories(${NAME} PUBLIC ${PROJECT_SOURCE_DIR}/include ${ION_BB_INCLUDE_DIRS} ${OpenCV_INCLUDE_DIRS})
