c72a6a4866f307a54e6784e66f6a5def7a397515
Xinyu Li <xinyuli@xinyus-air.lan>
2024-01-22 11:27:47 -0800

    type check for bool on param

--- python/ionpy/Param.py
+++ python/ionpy/Param.py
@@ -10,20 +10,24 @@ from .native import (
 
 class Param:
     def __init__(self,
-        key: Optional[str] = None,
-        val: Any = None,
-        # -- or
-        obj_: Optional[c_ion_param_t] = None,
-    ):
+                 key: Optional[str] = None,
+                 val: Any = None,
+                 # -- or
+                 obj_: Optional[c_ion_param_t] = None,
+                 ):
         if obj_ is None:
             obj_ = c_ion_param_t()
-
-            ret = ion_param_create(ctypes.byref(obj_), key.encode(), str(val).lower().encode())
+            if isinstance(val, bool):
+                if val:
+                    val = "true"
+                else:
+                    val = "false"
+            ret = ion_param_create(ctypes.byref(obj_), key.encode(), str(val).encode())
             if ret != 0:
                 raise Exception('Invalid operation')
 
         self.obj = obj_
 
     def __del__(self):
-        if self.obj: # check not nullptr
+        if self.obj:  # check not nullptr
             ion_param_destroy(self.obj)
