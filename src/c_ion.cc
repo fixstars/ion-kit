64df6061d99649e6874559136c24918ce904b513
Xinyu Li <xinyuli@Xinyus-MacBook-Air.local>
2024-03-04 11:16:52 -0800

    change ion_graph_create_with_multiple

--- src/c_ion.cc
+++ src/c_ion.cc
@@ -1081,11 +1081,23 @@ int ion_graph_add_node(ion_graph_t obj, const char *name, ion_node_t *node_ptr)
 }
 
 int ion_graph_create_with_multiple(ion_graph_t * ptr, ion_graph_t* objs, int size) {
-     auto sum_graph = *reinterpret_cast<Graph*>(objs[0]);
-    for (size_t i = 1; i < size; ++i) {
-       sum_graph = sum_graph + *reinterpret_cast<Graph*>(objs[i]);
+    try {
+        auto sum_graph = *reinterpret_cast<Graph*>(objs[0]);
+        for (size_t i = 1; i < size; ++i) {
+        sum_graph = sum_graph + *reinterpret_cast<Graph*>(objs[i]);
+        }
+        *ptr = reinterpret_cast<ion_graph_t>(new Graph(sum_graph));
+    } catch (const Halide::Error& e) {
+        log::error(e.what());
+        return 1;
+    } catch (const std::exception& e) {
+        log::error(e.what());
+        return 1;
+    } catch (...) {
+        log::error("Unknown exception was happened");
+        return 1;
     }
-    *ptr = reinterpret_cast<ion_graph_t>(new Graph(sum_graph));
+
     return 0;
 }
 
