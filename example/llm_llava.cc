71ca6da4642342df16eb67ff63c32276b9b4664d
Takuro Iizuka <t_iizuka@fixstars.com>
2024-04-18 16:48:00 +0000

    WIP

--- example/llm_llava.cc
+++ example/llm_llava.cc
@@ -15,18 +15,28 @@ int main(int argc, char *argv[]) {
         b.with_bb_module("ion-bb");
 
         auto n_img = b.add("image_io_color_data_loader").set_param(Param("url", "http://www.onthejob.education/images/4th_level/Road_Worker/Road_Worker_Darwin.jpg"), Param("width", width), Param("height", height));
-        // auto n_txt = b.add("llm_llava")(n["output"]);
-        // n = b.add("llm_draw_text")(n_img["output"], n_txt["output"])
-        auto n = b.add("image_io_gui_display")(n_img["output"]).set_param(Param{"width", width}, Param{"height", height});
+        auto n_txt = b.add("llm_llava")(n_img["output"]);
+#if 0
+        auto n = b.add("llm_draw_text")(n_img["output"], n_txt["output"]);
+        n = b.add("image_io_gui_display")(n["output"]).set_param(Param{"width", width}, Param{"height", height});
 
-	auto r = ion::Buffer<int>::make_scalar();
-	n["output"].bind(r);
+        auto r = ion::Buffer<int>::make_scalar();
+        n["output"].bind(r);
 
-        while (cv::waitKey(1)) {
+        for (int i=0; i<100; ++i) {
             b.run();
+            cv::waitKey(1);
         }
 
         std::cout << "llava example done!!!" << std::endl;
+#else
+        ion::Buffer<uint8_t> text_output{1024};
+        n_txt["output"].bind(text_output);
+
+        b.run();
+
+        std::cout << reinterpret_cast<const char*>(text_output.data()) << std::endl;
+        #endif
 
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
--- src/bb/llm/bb.h
+++ src/bb/llm/bb.h
@@ -7,13 +7,19 @@ namespace ion {
 namespace bb {
 namespace llm {
 
-class X : public BuildingBlock<X> {
+class Llava : public BuildingBlock<Llava> {
 public:
     Input<Halide::Func> input{"input", Halide::type_of<float>(), 3};
-    Output<Halide::Func> output{"output", Halide::type_of<float>(), 3};
+    Input<Halide::Func> prompt{"prompt", Halide::type_of<uint8_t>(), 1};
+    Output<Halide::Func> output{"output", Halide::type_of<uint8_t>(), 1};
 
     void generate() {
-        output(c, x, y) = input(x, y, c);
+        std::vector<ExternFuncArgument> params = {prompt};
+        Func llava("llava");
+        llava.define_extern("ion_llm_llava", params, Halide::type_of<uint8_t>(), 1);
+        llava.compute_root();
+
+        output = llava;
     }
 
 private:
@@ -24,6 +30,6 @@ private:
 }  // namespace bb
 }  // namespace ion
 
-ION_REGISTER_BUILDING_BLOCK(ion::bb::llm::X, llm_x);
+ION_REGISTER_BUILDING_BLOCK(ion::bb::llm::Llava, llm_llava);
 
 #endif  // ION_BB_LLM_BB_H
