da42f28f535d27c070e9a64c5e39a7fdd4d545e2
Takuro Iizuka <iitaku@gmail.com>
2024-04-29 18:23:23 -0700

    Improved layout

--- app/demo.py
+++ app/demo.py
@@ -8,10 +8,16 @@ from tkinter.ttk import *
 import sv_ttk
 from PIL import ImageTk, Image
 
-class Application:
-    def __init__(self, window, window_title):
+class App(Frame):
+    def __init__(self, window):
+        super().__init__(window, padding=15)
+
+        # self.rowconfigure(0, weight=1)
+        # self.rowconfigure(1, weight=1)
+        # self.columnconfigure(0, weight=1)
+        # self.columnconfigure(1, weight=1)
+
         self.window = window
-        self.window.title(window_title)
 
         # Model
         self.width = 1280
@@ -22,15 +28,16 @@ class Application:
 
         # View model
         self.prompt_string = StringVar()
-        self.prompt_string.set('Exlain the image in a single sentence.')
+        self.prompt_string.set('Explain the image in a single sentence.')
 
         # Support variables 
         self.live_mode = False
+        self.advanced_mode = False
         self.analyze_in_progress = False
         self.last_response = np.copy(self.response)
 
         self.init_pipeline()
-        self.init_layout(window)
+        self.init_layout()
 
         self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
 
@@ -43,7 +50,6 @@ class Application:
         params = [Param("num_devices", 1), Param("realtime_diaplay_mode", True)]
         n_img_cwh = self.b.add("image_io_u3v_cameraN_u8x3").set_param(params)
 
-        self.update_prompt()
         self.prompt_buf = Buffer(array=self.prompt)
         prompt_port = Port(name="prompt", type=Type(TypeCode.Int, 8, 1), dim=1)
         prompt_port.bind(self.prompt_buf)
@@ -61,26 +67,34 @@ class Application:
         self.response_buf = Buffer(array=self.response)
         n_txt.get_port("output").bind(self.response_buf)
     
-    def init_layout(self, window):
-        self.img_canvas = Canvas(window, width = self.width, height = self.height)
+    def init_layout(self):
+        img_frame = Frame(self, style='Card.TFrame', padding=15)
+        img_frame.grid(row=0, column=0, sticky='nsew')
+        self.img_canvas = Canvas(img_frame, width = self.width, height = self.height)
         self.img_canvas.pack()
 
-        self.prompt_textbox = Entry(window, textvariable=self.prompt_string)
+        control_frame = Frame(self, padding=15)
+        control_frame.grid(row=1, column=0, sticky='nsew')
+        self.prompt_textbox = Entry(control_frame, textvariable=self.prompt_string, width=100, font=('Helvetica', 18))
         self.prompt_textbox.bind("<FocusOut>", lambda event: self.update_prompt())
-        self.prompt_textbox.pack()
+        self.prompt_textbox.grid(row=0, column=0, columnspan=10, padx=5)
 
-        self.response_label = Label(window)
+        self.live_checkbutton = Checkbutton(control_frame, text='Live', style='Switch.TCheckbutton', command=self.toggle_live)
+        self.live_checkbutton.grid(row=0, column=11, padx=5)
+        
+        self.analysis_button = Button(control_frame, text='Analyze', command = self.analyze, width=10, style='Accent.TButton')
+        self.analysis_button.grid(row=0, column=12, padx=5)
+ 
+        response_frame = Frame(self, padding=15)
+        response_frame.grid(row=2, columnspan=2, sticky='nsew')
+        self.response_label = Label(response_frame, font=('Helvetica', 18), wraplength=1200, justify='left')
         self.response_label.pack()
 
-        self.live_checkbutton = Checkbutton(window, text='Live', style='Switch.TCheckbutton', command=self.toggle_live)
-        self.live_checkbutton.pack()
-        
-        self.analysis_button = Button(window, text='Analyze', command = self.analyze)
-        self.analysis_button.pack()
- 
-        self.update()
+        self.update_prompt()
+        self.update_response()
+        self.update_periodic()
   
-    def update(self):
+    def update_periodic(self):
         # Running pipeline
         self.b.run()
         
@@ -90,13 +104,23 @@ class Application:
         if (self.live_mode):
             self.update_response()
 
-        self.window.after(30, self.update)
+        self.window.after(30, self.update_periodic)
  
     def update_prompt(self):
-        print('prompt is updated')
         self.prompt.fill(0)
+        i = 0
+        if not self.advanced_mode:
+            # Append image prompt marker
+            for c in '<image>':
+                self.prompt[i] = ord(c)
+                i += 1
+        offset = i
         for i, c in enumerate(self.prompt_string.get()):
-            self.prompt[i] = ord(c)
+            self.prompt[offset+i] = ord(c)
+        
+        # Clearing response make look & feel better
+        self.response.fill(0)
+        self.response_label.configure(text='')
 
     def update_response(self):
         self.response_label.configure(text=''.join([chr(v) for v in self.response]))
@@ -125,9 +149,7 @@ class Application:
 
 # GUIウィンドウの作成
 root = Tk()  # create CTk window like you do with the Tk window
-
-root.geometry("1600x1200")
-app = Application(root, "Fixstars Demo")
-
+root.title("Interactive LLAVA Demo")
 sv_ttk.set_theme("dark")
+App(root).pack(expand=True, fill='both')
 root.mainloop()
